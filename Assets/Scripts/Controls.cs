1fc000b451b44f85681d340d89fcdb9d323ac2f3
Tsinoyboi <ajsnoface@gmail.com>
2015-04-05 14:51:51 -0700

    music added
    yes

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -5,29 +5,32 @@ public class Controls : MonoBehaviour {
 
 	//[SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
 
-	private Rigidbody2D m_Rigidbody2D;
-	public float maxSpeed = 10f;
-	public float maxTorque = 5f;
+    private Rigidbody2D m_Rigidbody2D;
+	public float maxSpeed = 5f;
+	public float maxTorque = 2f;
+    
+    public AudioClip crash1;
+
+    private AudioSource audioSource;
 
 	private void Awake()
 	{
 		// Setting up references.
 		m_Rigidbody2D = GetComponent<Rigidbody2D>();
-	}
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(crash1, 1F);
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//float move = Input.GetAxisRaw ("Vertical");
-		Vector2 axis = new Vector2 (-Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		float thrust = Input.GetAxisRaw ("Vertical");
+        //if (thrust != 0)
+            audioSource.PlayOneShot(crash1,1F);
 		//m_Rigidbody2D.velocity = new Vector2 (m_Rigidbody2D.velocity.x, move * maxSpeed);
-		m_Rigidbody2D.AddRelativeForce(new Vector2 (0f, axis.y * maxSpeed));
+		m_Rigidbody2D.AddRelativeForce(new Vector2 (0f, thrust * maxSpeed));
 		//m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-		//float turn = -Input.GetAxis ("Horizontal");
-		m_Rigidbody2D.AddTorque(axis.x * maxTorque);
+		float turn = -Input.GetAxisRaw ("Horizontal");
+		m_Rigidbody2D.AddTorque(turn * maxTorque);
 	}
 }
---
+++ Assets/Scripts/CrashSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrashSound : MonoBehaviour {
+
+    public AudioClip crash1;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () {
+
+        audioSource = GetComponent<AudioSource>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        audioSource.PlayOneShot(crash1, 0.7F);
+    }
+}
